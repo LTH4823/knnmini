49609ad2c895498f4e0495ffd98b581c62ebdc7b
LTH4823 <peter4823zx@naver.com>
2022-02-17 21:25:21 +0900

    update knn min code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,19 +13,19 @@ public class Main {
         Point minPoint = null;
 
         Point[] arr = new Point[6];
-        Point kick001 = new Point("kick01",1,10,"action");
-        Point kick002 = new Point("kick01",2,7,"action");
-        Point kick003 = new Point("kick01",13,13,"action");
-        Point romance001 = new Point("kick01",13,1,"action");
-        Point romance002 = new Point("kick01",7,3,"action");
-        Point romance003 = new Point("kick01",10,2,"action");
+        Point movie0 = new Point("kick01",1,10,"action");
+        Point movie1 = new Point("kick01",2,7,"action");
+        Point movie2 = new Point("kick01",13,13,"action");
+        Point movie3 = new Point("kick01",13,1,"action");
+        Point movie4 = new Point("kick01",7,3,"action");
+        Point movie5 = new Point("kick01",10,2,"action");
 
-        arr[0] = kick001;
-        arr[1] = kick002;
-        arr[2] = kick003;
-        arr[3] = romance001;
-        arr[4] = romance002;
-        arr[5] = romance003;
+        arr[0] = movie0;
+        arr[1] = movie1;
+        arr[2] = movie2;
+        arr[3] = movie3;
+        arr[4] = movie4;
+        arr[5] = movie5;
 
         System.out.println("영화 명을 입력해 주십시오.");
         name = scanner.nextLine();
@@ -46,6 +46,6 @@ public class Main {
             }
         }
         target.kind = minPoint.kind;
-        System.out.println("입력하신 영화 "+target.name+"은 "+target.kind+" 영화입니다.");
+        System.out.println("\n입력하신 영화 "+target.name+"은 "+target.kind+" 영화입니다.");
     }
 }
